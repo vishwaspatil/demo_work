04a999c0a77421c4e1d6ea0f75d16aa70aeb37f4
vishwaspatil <vsp@narola.email>
2016-07-29 16:25:02 +0530

    changes related to devise

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap/bootstrap.min.js
+//= require raphel/raphael-min.js
+//= require plugins/morris.min.js
+//= require plugins/jquery.sparkline.min.js
+//= require plugins/jquery-jvectormap-1.2.2.min.js
+//= require plugins/jquery-jvectormap-world-mill-en.js
+//= require plugins/jquery.knob.js
+//= require moment/moment.min.js
+//= require plugins/daterangepicker.js
+//= require plugins/bootstrap-datepicker.js
+//= require plugins/bootstrap3-wysihtml5.all.min.js
+//= require plugins/jquery.slimscroll.min.js
+//= require plugins/fastclick.js
+//= require dist/app.min.js
+//  require dist/dashboard
+// require dist/demo.js
+
+
+// require turbolinks
+// require_tree .
